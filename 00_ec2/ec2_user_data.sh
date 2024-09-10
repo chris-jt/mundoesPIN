177755956b5334a6c810ff3c04ef0a4245913c73
chris-jt <chtronconi@gmail.com>
2024-09-10 14:16:06 -0300

    modificando script

--- 00_ec2/ec2_user_data.sh
+++ 00_ec2/ec2_user_data.sh
@@ -6,14 +6,14 @@ unzip -v
 echo "Installing AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+sudo ./aws/install --update
 aws --version
 
 echo "Installing kubectl"
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.2/2024-07-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+#echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --client
 
 echo "Installing ekctl"
@@ -21,11 +21,13 @@ echo "Installing ekctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 export PATH=$PATH:/usr/local/bin
-echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+#echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
 eksctl version
 
 echo "Installing docker"
-sudo apt install -y docker
+sudo snap install docker
+sudo addgroup --system docker
+sudo adduser $USER docker
 sudo usermod -a -G docker ec2-user
 newgrp docker
 wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
@@ -35,7 +37,7 @@ sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
 echo "Installing Helm"
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 chmod 700 get_helm.sh
 ./get_helm.sh
 helm version
